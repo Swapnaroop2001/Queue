7fdae31da43cd0bbc3618b65bb93a872bb5bc511
Swapnaroop <swapnaroop2001@gmail.com>
2023-11-22 12:02:22 -0500

    New queue

---
+++ Queue.java
@@ -0,0 +1,12 @@
+import java.util.*;
+/**
+ * Queue
+ */
+public class Queue {
+    Queue<String> queue = new LinkedList<>();
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
