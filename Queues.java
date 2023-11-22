5fffd6d69294c16a2200f5505d0fca74eb97637c
Swapnaroop <swapnaroop2001@gmail.com>
2023-11-22 15:18:34 -0500

    Using Queue class

---
+++ Queues.java
@@ -0,0 +1,19 @@
+import java.util.*;;
+/**
+ * Queues
+ */
+public class Queues {
+
+    public static void main(String[] args) {
+        Queue <String> ab = new LinkedList<String>();
+        ab.add("Apple");
+        ab.add("Meta");
+        ab.add("Google");
+        ab.add("Aamazon");
+        ab.add("Netflix");
+        
+        System.out.println(ab);
+    }
+    
+    
+}
