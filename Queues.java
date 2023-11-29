77dd259af1e6f6c7014378b9953f9c43dfed2d06
Swapnaroop <swapnaroop2001@gmail.com>
2023-11-29 00:36:17 -0500

    dailyTemperatures(int[] temperatures)

--- Queues.java
+++ Queues.java
@@ -4,13 +4,32 @@ import java.util.*;;
  */
 public class Queues {
 
+    Queue<Integer> q = new LinkedList<Integer>(); 
+    
+    void push(int val) 
+    {
+        int size = q.size();
+        q.add(val);
+         
+        for (int i = 0; i < size; i++) 
+        {
+            int x = q.remove();
+            q.add(x);
+        }
+    }
+
+
     public static void main(String[] args) {
         Queue <String> ab = new LinkedList<String>();
+        
         ab.add("Apple");
         ab.add("Meta");
         ab.add("Google");
         ab.add("Aamazon");
         ab.add("Netflix");
+
+        System.out.println(ab.element());
+
         
         System.out.println(ab);
     }
